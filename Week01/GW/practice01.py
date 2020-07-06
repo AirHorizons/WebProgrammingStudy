6b4496523bda54fb3b9db8e89f71598b4f1dd33c
stroller1010 <qkrjsdn1010@naver.com>
2020-07-06 11:47:15 +0900

    Add practice file

---
+++ Week01/GW/practice01.py
@@ -0,0 +1,12 @@
+i = 99
+while i> 1:
+    print(i,'bottles bottles of beer on the wall, ', i, 'bottles of beer.')
+    print('Take one down and pass it around, ', i-1, ' bottles of beer on the wall.\n\n')
+    i-=1
+print('1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n')
+
+print('No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall')
+
+# 뭔가 0 대신 No more가 나오게 하는 방법이 있을 것 같은데... 
+
+    
