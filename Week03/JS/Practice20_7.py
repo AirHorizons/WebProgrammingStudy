80550f7525d9ff6937af42d5779ef3e074558cd1
JS Kim <jskim3889@gmail.com>
2020-07-25 23:42:07 +0900

    Update Practice20_7.py
    가르쳐주십시오

--- Week03/JS/Practice20_7.py
+++ Week03/JS/Practice20_7.py
@@ -11,21 +11,18 @@
 
 #실습문제
 
-a, b = input().split()
+a, b = map(int, input().split())
 if a > b:
     print('a는 b보다 작게 입력하세요!')
 else:
-    if (1<=a<=1000 and 10<=b<=1000):
-        a = int(a)
-        b = int(b)
-    else:
+    if not (1 <= a <= 1000 and 10 <= b <= 1000):
         print('a는 1~1000, b는 10~1000 범위')
-for i in range(a, b):
-    if i % 5 == 0 and i % 7  == 0:
+for i in range(a, b + 1):
+    if i % 5 == 0 and i % 7 == 0:
         print('FizzBuzz')
     elif i % 5 == 0:
         print('Fizz')
-    elif i % 7 ==0:
+    elif i % 7 == 0:
         print('Buzz')
     else:
         print(i)
