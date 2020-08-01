c3a369ac54587badcca42775baccfc24da0a84be
JS Kim <jskim3889@gmail.com>
2020-08-02 00:07:12 +0900

    시지프스의 연습문제
    열심히 올리는 중

---
+++ Week03/JS/practice23_7.py
@@ -0,0 +1,2 @@
+a = [[[i for i in range(2)] for j in range(4)] for k in range(3)]
+print(a)
---
+++ Week03/JS/practice24_4.py
@@ -0,0 +1,11 @@
+#연습문제
+#path = 'C:\\Users\\dojang\\AppData\\Local\\Programs\\Python\\Python36-32\\python.exe'
+#a = path.split('\\')
+#filename = a[-1]
+#print(filename)
+
+#심사문제
+countthe = input()
+count = countthe.count('the')
+print(count)
+
